86a7877f9ed9183d4a92cf04d42d1b6df88021cb
iFew <chitpong@gmail.com>
2017-11-15 11:26:17 +0700

    create class by automatic

---
+++ FizzBuzz.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace _04_fizzbuzz_testdouble
+{
+    internal class FizzBuzz
+    {
+        public FizzBuzz()
+        {
+        }
+
+        internal string GetNumber(int v)
+        {
+            
+        }
+    }
+}
