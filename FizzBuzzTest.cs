8a3c34e7719dac2e72d6a78879cfb56363ce7d6c
iFew <chitpong@gmail.com>
2017-11-15 11:37:57 +0700

    refactor

--- FizzBuzzTest.cs
+++ FizzBuzzTest.cs
@@ -5,12 +5,13 @@ namespace _04_fizzbuzz_testdouble
 {
     public class FizzBuzzTest
     {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        
         [Fact]
         public void InputOneGetOne()
         {
             string expected = "1";
 
-            FizzBuzz fizzbuzz = new FizzBuzz();
             string actual = fizzbuzz.GetNumber(1);
 
             Assert.Equal(expected, actual);
@@ -21,7 +22,6 @@ namespace _04_fizzbuzz_testdouble
         {
             string expected = "2";
 
-            FizzBuzz fizzbuzz = new FizzBuzz();
             string actual = fizzbuzz.GetNumber(2);
 
             Assert.Equal(expected, actual);
